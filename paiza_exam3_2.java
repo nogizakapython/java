fec9e38dd2c4ddfc23a4c7e7861a917dc52b18d6
Takao Hattori <nogizakapython@outlook.jp>
2024-01-10 17:17:26 +0900

    Modefy20240110Morning

---
+++ paiza_exam3_2.java
@@ -0,0 +1,6 @@
+public class paiza_exam3_2 {
+    public static void main(String... args) {
+        System.out.println("こんにちは");
+        System.out.println("こんばんは");
+    }
+}
---
+++ paiza_exam3_3.java
@@ -0,0 +1,6 @@
+public class paiza_exam3_3 {
+    public static void main(String... args) {
+        System.out.print("こんにちは");
+        System.out.print("こんばんは");
+    }
+}
