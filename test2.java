65c803c269f94d3748e648e1407054ef677ac9c3
Takao Hattori <nogizakapython@outlook.jp>
2024-01-10 18:10:30 +0900

    Modefy20240110eve

---
+++ test2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String... args) {
+        Scanner scan = new Scanner(System.in);
+        String msg1 = scan.nextLine();
+
+        System.out.println(msg1);
+        scan.close();
+
+    }
+}
