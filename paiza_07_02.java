f80ab368b4173c87c5c2f31ab30f59e1dd7fc30a
Takao Hattori <nogizakapython@outlook.jp>
2024-01-15 04:12:50 +0900

    Modefy20240115Morning

---
+++ paiza_07_02.java
@@ -0,0 +1,10 @@
+public class paiza_07_02 {
+  public static void main(String... args) {
+      final Double tax = 1.1;
+      Integer book = 1000;
+      Double sum = book * tax;
+      System.out.println("本の定価は" + book + "円です");
+      System.out.println("本の税込価格は" + sum + "円です");
+
+  }
+}
---
+++ paiza_ex07_01.java
@@ -0,0 +1,12 @@
+public class paiza_ex07_01 {
+  public static void main(String... args) {
+      Integer price = 200;
+      Integer totalPrice = price * 5;
+      String product = "オレンジ";
+
+      System.out.println(product + "の値段は" + price+ "円です");
+      System.out.println( "総額は" + totalPrice + "円です");
+
+
+  }
+}
