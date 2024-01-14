bab9103e36a9806d4b501b6c9d8b1963cf721b12
Takao Hattori <nogizakapython@outlook.jp>
2024-01-14 15:47:41 +0900

    Modefy20240114java

---
+++ paiza_ex06_04.java
@@ -0,0 +1,11 @@
+public class paiza_ex06_04 {
+  public static void main(String... args) {
+      String msg1 = "paiza";
+      String m1sg = "paiza";
+      Integer num = 20;
+      System.out.println(msg1);
+      System.out.println(m1sg);
+      System.out.println(num);
+
+  }
+}
