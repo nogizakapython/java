27f76504e891950931ce16f9046613d6f3464e29
Takao Hattori <nogizakapython@outlook.jp>
2024-01-22 06:02:43 +0900

    Modefy20240122

---
+++ paiza_09_02.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class paiza_09_02 {
+    public static void main(String... args) {
+        ArrayList<String> members = new ArrayList<>();
+        members.add("kirishima");
+        System.out.println(members);
+    }
+}
