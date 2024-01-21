a6c557d8ed1611d824f2343342c3db50ae41aff6
Takao Hattori <nogizakapython@outlook.jp>
2024-01-22 06:03:11 +0900

    Modefy20240122

---
+++ paiza_ex09_02.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class paiza_ex09_02 {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        System.out.println(fruits);
+    }
+}
