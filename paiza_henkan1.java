9206b5c72a859b51147b93e21684f4d1a56cd516
Takao Hattori <nogizakapython@outlook.jp>
2024-01-24 15:19:33 +0900

    Modefy20240124

---
+++ paiza_henkan1.java
@@ -0,0 +1,14 @@
+public class paiza_henkan1 {
+  public static void main(String[] args) {
+      // 自分の得意な言語で
+      // Let's チャレンジ！！
+
+      //String line = sc.nextLine();
+      int A = 437326;
+      int B = 9085;
+      int ansA = A / B;
+      int ansB = A % B;
+      //  ラッパークラスを用いて数値を文字列に変換して出力する
+      System.out.println(Integer.valueOf(ansA).toString() + " " + Integer.valueOf(ansB).toString());
+  }
+}
