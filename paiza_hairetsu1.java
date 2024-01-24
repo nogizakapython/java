8a51bad67a33c251caa850ed36abdceafbedf9d6
Takao Hattori <nogizakapython@outlook.jp>
2024-01-24 14:53:49 +0900

    Modefy20240124

---
+++ paiza_hairetsu1.java
@@ -0,0 +1,16 @@
+//import java.util.*;
+
+
+public class paiza_hairetsu1 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        // Scanner sc = new Scanner(System.in);
+        int[] array1 = {8,1,3};
+
+        for(int j:array1){
+            System.out.println(j);
+        }
+
+    }
+}
