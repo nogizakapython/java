ce6aa3ab1a440bc78b4ebb30c68ab5f99b812f71
Takao Hattori <nogizakapython@outlook.jp>
2024-01-26 07:34:07 +0900

    Modefy20240126

---
+++ paiza_prob9.java
@@ -0,0 +1,18 @@
+// import java.util.*;
+
+
+public class paiza_prob9 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        // Scanner sc = new Scanner(System.in);
+        //String line = sc.nextLine();
+        int A = 202;
+        int B = 134;
+        int C = 107;
+        int ans;
+        ans = (A+B)*C;
+        ans = ans * ans;
+        System.out.println(ans);
+    }
+}
