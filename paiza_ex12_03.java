4250c711b9f9f67a39dc73ffc46e72727d0ad36b
Takao Hattori <nogizakapython@outlook.jp>
2024-01-31 12:49:47 +0900

    Modefy20240131

---
+++ paiza_ex12_03.java
@@ -0,0 +1,11 @@
+
+public class paiza_ex12_03 {
+    public static void main(String... args) {
+      Integer i = 0;
+
+      if(i == 0){
+          System.out.println("iは0です。");
+      }
+
+    }
+}
