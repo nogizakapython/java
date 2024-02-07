f8f72edce4bbb13c7807b58049ac33ce15a6a787
Takao Hattori <nogizakapython@outlook.jp>
2024-02-08 07:39:06 +0900

    Modefy20240208

---
+++ paiza_12_05.java
@@ -0,0 +1,13 @@
+public class paiza_12_05 {
+  public static void main(String... args) {
+      Integer i = 2;
+
+      if (i == 1) {
+          System.out.println("1位です");
+      } else if ( i == 2){
+          System.out.println("2位です");
+      } else {
+          System.out.println("最下位です");
+      }
+  }
+}
