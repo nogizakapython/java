eb71f4383b3d1769104205fa775c0c0b7fc41efe
Takao Hattori <nogizakapython@outlook.jp>
2024-02-08 07:36:43 +0900

    Modefy20240208

---
+++ paiza_ex12_05.java
@@ -0,0 +1,14 @@
+public class paiza_ex12_05 {
+  public static void main(String... args) {
+
+      Integer i = 2;
+
+      if (i == 0) {
+          System.out.println("iは0です");
+      } else if ( i == 1){
+          System.out.println("iは1です");
+      } else {
+          System.out.println("iは0でも1でもありません");
+      }
+  }
+}
