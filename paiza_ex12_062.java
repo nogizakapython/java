eda0f49ce57756d0c3f4bb964a693850d3086d2d
Takao Hattori <nogizakapython@outlook.jp>
2024-02-15 12:37:02 +0900

    Modefy20240215

---
+++ paiza_ex12_062.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class paiza_ex12_062 {
+    public static void main(String... args) {
+        ArrayList<Integer> scores = new ArrayList<>();
+        scores.add(1);
+        scores.add(2);
+        scores.add(3);
+        scores.add(4);
+        scores.add(5);
+
+        for(Integer score : scores){
+            if (score % 2 == 0){
+                System.out.println(score + "は偶数");
+            } else {
+                System.out.println(score + "は奇数");
+            }
+        }
+    }
+}
