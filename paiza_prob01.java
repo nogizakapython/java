f3ed6c3d35b6e45fb82ef7b461cda1bed72d4ef9
Takao Hattori <nogizakapython@outlook.jp>
2024-02-22 07:54:15 +0900

    Modefy20240222

---
+++ paiza_prob01.java
@@ -0,0 +1,14 @@
+public class paiza_prob01 {
+  public static void main(String[] args) {
+      // 自分の得意な言語で
+      // Let's チャレンジ！！
+
+      //String line = sc.nextLine();
+      int N = 0;
+      N += 3286;
+      N *= 4736;
+      N %= 12312;
+      //  ラッパークラスを用いて数値を文字列に変換して出力する
+      System.out.println(N);
+  }
+}
---
+++ paiza_prob01_08.java
@@ -0,0 +1,12 @@
+public class Paiza_Prob08 {
+  public static void main(String[] args) {
+      // 自分の得意な言語で
+      // Let's チャレンジ！！
+      Integer N;
+      N = 0;
+      N += 3286;
+      N *= 4736;
+      N %= 12312;
+      System.out.println(N);
+  }
+}
