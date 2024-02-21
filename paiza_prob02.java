17db29b64f6f1be85e9408b8824b04a561f74277
Takao Hattori <nogizakapython@outlook.jp>
2024-02-22 07:59:09 +0900

    Modefy20240222

---
+++ paiza_prob02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class paiza_prob02 {
+  public static void main(String[] args) {
+      // 自分の得意な言語で
+      // Let's チャレンジ！！
+      Scanner sc = new Scanner(System.in);
+      String line = sc.nextLine();
+      System.out.println(line);
+  }
+}
