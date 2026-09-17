eaccc908dbf26326a4488a0769bcd2691eca9a6b
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-09-17 16:52:58 +0900

    Modefy20260917

---
+++ paiza_dtest14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] array1 = line.split(" ");
+        for(String s : array1){
+            System.out.println(s);
+        }
+        //System.out.println("XXXXXX");
+    }
+}
