5574730fbdfec2c44840bdbcb387ec782290471a
meitetsu5000 <nogizakapython@outlook.jp>
2026-09-18 11:14:22 +0900

    Modefy20260918

--- paiza_dtest13.java
+++ paiza_dtest13.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 
-public class Main {
+public class paiza_dtest13 {
     public static void main(String[] args) {
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
+
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         System.out.println(s);
